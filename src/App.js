aa0140d5c887c2725eae0b25c75bcaa68ed38a7a
unknown <moreno7046@hotmail.com>
2020-06-04 17:30:55 -0500

    finished react-counter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Counter from './Components/Counter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header className='App-header App-link'>
         <h2>Counter App</h2>
       </header>
-      <Counter/>
+      <Counter />
     </div>
   );
 }
